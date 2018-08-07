8fed145553cbeb41a20655c78c7f7cd8f439a2f2
Bruno Bertolini <dev@brunobertolini.com>
2018-08-06 21:18:33 -0300

    refactor: use rollup to build and uglify to cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
-import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
+import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify";
 
 export default {
-	entry: 'src/index.js',
-	moduleName: 'styledBy',
-	exports: 'named',
+	entry: "src/index.js",
+	moduleName: "styledBy",
+	exports: "named",
 	targets: [
 		{
-			dest: 'build/styled-by.min.js',
-			format: 'iife'
+			dest: "build/styled-by.min.js",
+			format: "iife"
+		},
+		{
+			dest: "lib/index.js",
+			format: "cjs"
 		}
 	],
 	plugins: [babel(), uglify()]
-}
+};
